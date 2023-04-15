a0d889e1fb516bd2ea4db0d1025df6a5518fe339
Hatice Karaca <haticekrc772@gmail.com>
2023-04-15 17:25:33 +0300

    turtleDrawingProject added

---
+++ main.py
@@ -0,0 +1,24 @@
+import turtle
+
+drawing_board=turtle.Screen()
+drawing_board.bgcolor("light blue")
+drawing_board.title("Python Turtle")
+
+"""turtle_instance=turtle.Turtle()
+turtle_instance.forward(100)
+
+turtle_instance2=turtle.Turtle()
+turtle_instance2.left(90)
+turtle_instance2.forward(100)
+"""
+
+#square
+"""turtle_instance=turtle.Turtle()
+turtle_instance.color("purple")
+i=1
+while(i<5):
+    turtle_instance.forward(100)
+    turtle_instance.left(90)
+    i+=1
+"""
+turtle.done()  # Keep the turtle window open until it is manually closed
