5d9edb98a7c6494dab19cb91b12899387be2db48
Hatice Karaca <haticekrc772@gmail.com>
2023-04-15 17:36:34 +0300

    star added

--- main.py
+++ main.py
@@ -21,4 +21,11 @@ while(i<5):
     turtle_instance.left(90)
     i+=1
 """
+
+#star
+turtle_instance=turtle.Turtle()
+turtle_instance.color("red")
+for i in range(5):
+    turtle_instance.right(144)
+    turtle_instance.forward(300)
 turtle.done()  # Keep the turtle window open until it is manually closed
